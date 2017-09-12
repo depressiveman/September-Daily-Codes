4241b21a53b65c04246dc36f91265cfadec5e007
Game110AM <Game110AM@605-110-STU07>
2017-09-12 08:14:21 -0600

    Add project files.

---
+++ daily code 9-12-17/daily code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input;
+	
+
+	cout << "what is your score bude in your favorite bame." << endl;
+	cin >> input;
+	
+
+			if (input <= 50)
+				cout << "you are a bidiot" << endl;
+			else if (input <= 100)
+				cout << "you are be best" << endl;
+			else
+				cout << "no" << endl;
+
+}
